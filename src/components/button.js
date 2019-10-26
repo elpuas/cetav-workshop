bc306dd0ad786116caf4323adc6fab0dc4447b5e
Alfredo Navas <elpuas@gmail.com>
2019-10-25 21:42:13 -0600

    styled button, styled navbar

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const StyleLink = styled(Link)`
+	border-radius: 50px;
+	text-decoration: none;
+	color: white;
+	background: purple;
+	padding: 10px 25px;
+	margin-bottom: 1rem;
+	margin-bottom: 1rem;
+    display: block;
+    max-width: 300px;
+    text-align: center;
+    text-transform: uppercase;
+`
 
 const Button = (props) => (
-	<Link to={props.url}>
+	<StyleLink to={props.url}>
 		{props.name}
-	</Link>
+	</StyleLink>
 )
 export default Button
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,21 @@
 import { Link } from "gatsby"
 import React from "react"
 
+import styled from "styled-components"
+
+const Bar = styled.div`
+float: right;
+	a {
+		color: white;
+		margin: 10px;
+	}
+`
+
 const Navbar = () => (
-	<div>
+	<Bar>
 		<Link to="/">Inicio</Link>
 		<Link to="/page-2/">Page 2</Link>
 		<Link to="/acerca/">Acerca</Link>
-	</div>
+	</Bar>
 )
 export default Navbar
